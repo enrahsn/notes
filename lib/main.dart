546b573a6cf0e52b24f10758138167ddfc2d59fe
enrahsn <enrahsn@gmail.com>
2024-04-12 09:45:58 +0200

    modified theme

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter notes',
+      theme: ThemeData(brightness: Brightness.dark),
+      // theme: ThemeData
+      // theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // useMaterial3: true,
+      // .dark(),
+      // home: const MyHomePage(title: 'Flutter NOTES Page'),
+      home: const MyHomePage(title: 'Flutter NOTES Page'),
     );
   }
 }
